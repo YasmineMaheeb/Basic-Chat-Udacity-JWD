699e9fe13dc0f375fdb7dcd3565eae4254edb5d3
YasmineMaheeb <yasminemaheeb@gmail.com>
2020-07-25 02:50:31 +0200

    added signup test

---
+++ src/test/java/com/udacity/jwdnd/c1/review/pages/ChatPage.java
@@ -0,0 +1,4 @@
+package com.udacity.jwdnd.c1.review.pages;
+
+public class ChatPage {
+}
---
+++ src/test/java/com/udacity/jwdnd/c1/review/pages/SignupPage.java
@@ -0,0 +1,71 @@
+package com.udacity.jwdnd.c1.review.pages;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignupPage {
+
+    @FindBy(id = "inputFirstName")
+    private WebElement firstNameField;
+
+    @FindBy(id = "inputLastName")
+    private WebElement lastNameField;
+
+    @FindBy(id = "inputUsername")
+    private WebElement usernameField;
+
+    @FindBy(id = "inputPassword")
+    private WebElement passwordField;
+
+    @FindBy(className = "result")
+    private WebElement result;
+
+    public SignupPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void setFirstNameField(String fname) {
+        firstNameField.sendKeys(fname);
+    }
+
+    public void setLastNameField(String lname) {
+        lastNameField.sendKeys(lname);
+    }
+
+    public void setUsername(String username) {
+        usernameField.sendKeys(username);
+    }
+
+    public void clearUsername() {
+        usernameField.clear();
+    }
+
+    public void setPasswordField(String password) {
+        passwordField.sendKeys(password);
+    }
+
+    public String getResult(){
+            return result.getText();
+    }
+
+    public void submit(){
+        usernameField.submit();
+    }
+
+//    public int getDisplayedCount() {
+//        return Integer.parseInt(countDisplay.getText());
+//    }
+//
+//    public void incrementCount() {
+//        incrementButton.click();
+//    }
+//
+//    public void resetCount(int value) {
+//        resetValueField.clear();
+//        resetValueField.sendKeys(String.valueOf(value));
+//        resetButton.click();
+//    }
+}
